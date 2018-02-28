23c40a361a65314c8c46867a239cf92f517a5dac
Nathan Yang <nathanyang@engineering.ucla.edu>
2018-02-27 18:44:06 -0800

    Implemented blog post endpoint

--- routes/blog.js
+++ routes/blog.js
@@ -7,14 +7,32 @@ router.get('/', (req, res) => {
 });
 
 /* GET Post */
-router.get('/:username/:postid', function(req, res, next) {
+router.get('/:username/:postid', (req, res) => {
   const username = req.params.username;
   const postid = req.params.postid;
 
   var Posts = db.get().collection('Posts');
-  Posts.findOne({ "postid": 1 }).then(post => {
-		console.log(post);
-		res.send("Hi");
+  Posts.findOne({ "postid": parseInt(postid) }).then(post => {
+
+		res.render('post', {
+			title : post.title,
+			username: post.username,
+			body: post.body
+		});
+
+	}).catch((err) => {
+		console.log(err.message);
+	});
+});
+
+/* GET Posts List */
+router.get('/:username', (req, res) => {
+	const username = req.params.username;
+
+	var Posts = db.get().collection('Posts');
+	Posts.find({ "username": username }).toArray().then(postsList => {
+		console.log(postsList);
+		res.send("Posts List");
 	}).catch((err) => {
 		console.log("Error!");
 	});
